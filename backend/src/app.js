aac961167ef4dcf700bdad0605947bdbcb18f25e
Shivam Singh <fakegod436@gmail.com>
2026-03-13 16:49:10 +0530

    configure CORS to allow requests from the production frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,10 @@ const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
 
-app.use(cors());
+app.use( cors({
+    origin: "https://employee-management-9azq.vercel.app",
+    credentials: true
+  }));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 
